517a698edb5d571d1087d06ec37efece66e0883b
CristianMld <hkrcrm@gmail.com>
2022-11-25 13:21:15 -0300

    style 6

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import PokemonCard from "./PokemonCard";
 
-
 const Pokemons = () => {
 
   const userName = useSelector(state => state.name)
